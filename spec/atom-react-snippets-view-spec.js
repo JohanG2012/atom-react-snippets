f89948f627c1bf172a18cffa4d5c29bf2a57e114
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-05 06:16:38 +0200

    init

---
+++ spec/atom-react-snippets-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomReactSnippetsView from '../lib/atom-react-snippets-view';
+
+describe('AtomReactSnippetsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
